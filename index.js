eb3a20f5adc971967978060d08b8d771c71207f8
Cole Willsea <coleww@gmail.com>
2015-06-22 19:45:37 -0700

    implement the thing

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
-module.exports = function(string, constraint){
+module.exports = function(string, _c){
+  var result = true
+  var constraint
+
+  if(Array.isArray(_c)){
+    constraint = _c
+  } else if (_c){
+    constraint = ['b', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'p', 'q', 't', 'y']
+  }
+
+  var letters = string.toLowerCase().split("").filter(function(el){return el.match(/[a-z]/)})
+
+  var map = {}
+
+  letters.forEach(function(letter){
+    map[letter] = true
+  })
+
+  if(constraint){
+    constraint.forEach(function(letter){
+
+      // return map[letter] ? result = false : true
+      if(map[letter]) result = false
+    })
+  } else {
+    result = 'abcdefghijklmnopqrstuvwxyz'.split("").filter(function(letter){return !map[letter]})
+    if(!result.length) result = false
+  }
+  return result;
 }
